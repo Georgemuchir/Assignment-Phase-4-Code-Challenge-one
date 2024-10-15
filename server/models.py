acc93cf679a49a26a91c4aae105441e7054a6c59
Georgemuchiri <georgw.muchiri@student.moringaschool.com>
2024-10-15 09:45:22 +0300

    Make changes

--- server/models.py
+++ server/models.py
@@ -1,9 +1,11 @@
+
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
+print("Flask-SQLAlchemy imported successfully!")
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
